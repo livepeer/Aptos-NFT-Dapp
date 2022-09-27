c3bfce77abf1389de75270029de258afde4ab0cf
Shih-Yu <shih-yu@Admins-MacBook-Pro.local>
2022-09-27 15:36:56 -0500

    add upload asset page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { LivepeerConfig, createReactClient, studioProvider } from '@livepeer/react';
+
+const client = createReactClient({
+  provider: studioProvider({ apiKey: process.env.NEXT_PUBLIC_LIVEPEER_API }),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <LivepeerConfig client={client}>
+      <Component {...pageProps} />
+    </LivepeerConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
