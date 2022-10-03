a7efc1e235efe3c6c3ddcdd096fc07fbc73cbc59
Shih-Yu <shih-yu@Admins-MacBook-Pro.local>
2022-10-03 16:35:29 -0500

    update dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { LivepeerConfig, createReactClient, studioProvider } from '@livepeer/react';
 
+
 const client = createReactClient({
   provider: studioProvider({ apiKey: process.env.NEXT_PUBLIC_LIVEPEER_API }),
 });
